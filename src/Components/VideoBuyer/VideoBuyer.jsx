f5756266b2802ea8942c4b1e3121e2b18013ef7e
kanchankasniya <kanchankasniya@gmail.com>
2024-07-04 16:16:34 +0530

    videoBuyer created

--- src/Components/VideoBuyer/VideoBuyer.jsx
+++ src/Components/VideoBuyer/VideoBuyer.jsx
@@ -9,8 +9,18 @@ export default function VideoBuyer() {
             <img className='videopic' src="images/videopic.png" alt="" />
             <img className='vector' src="images/vector.png" alt="" />
         </div>
-        <div className="VideoBuyerContentRight"></div>
-      </div>
+        <div className="VideoBuyerContentRight">
+          <div className="VideoBuyerHeading">
+            <div className="buyer">Buyer</div>
+            <div className="supplier">Supplier</div>
+          </div>
+          <ul className="VideoBuyerList">
+            <li className="VideoBuyerListItems">Post your requirements.</li>
+            <li className="VideoBuyerListItems">Sit back for multiple suppliers to contact you.</li>
+            <li className="VideoBuyerListItems">Choose among the suppliers based on the ratings and reviews.</li>
+          </ul>
+        </div>
+      </div>f 
     </div>
   )
 }
